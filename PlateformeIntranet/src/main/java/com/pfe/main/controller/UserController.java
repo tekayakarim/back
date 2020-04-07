ccce53595dede38a49f565a2a11bc78fd7257798
tekayakarim <36500892+tekayakarim@users.noreply.github.com>
2020-04-07 22:00:59 +0200

    finish

---
+++ PlateformeIntranet/src/main/java/com/pfe/main/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.pfe.main.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pfe.main.entity.JwtUser;
+import com.pfe.main.service.EmployeService;
+
+@RestController
+@RequestMapping("/main/user")
+public class UserController {
+@Autowired
+EmployeService employeService;
+
+@GetMapping("/getCurrentUserEmploye")
+public JwtUser getUserEmploye(@RequestParam String userName) {
+	return employeService.getEmploye(userName);
+}
+}
