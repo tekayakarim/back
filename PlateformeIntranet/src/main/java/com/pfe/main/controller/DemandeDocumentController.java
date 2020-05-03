d51eae2164223e29256cdb1b9af32e8a708b2df4
hedi-code <hedibensafegine7@gmail.com>
2020-05-03 21:14:08 +0300

    badalt 7aja ala5ater kenet to5rejli error

--- PlateformeIntranet/src/main/java/com/pfe/main/controller/DemandeDocumentController.java
+++ PlateformeIntranet/src/main/java/com/pfe/main/controller/DemandeDocumentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import com.pfe.main.service.DemandeDocumentService;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/main/demandeDocument")
 public class DemandeDocumentController {
 
--- PlateformeIntranet/src/main/java/com/pfe/main/controller/UserController.java
+++ PlateformeIntranet/src/main/java/com/pfe/main/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.pfe.main.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import com.pfe.main.repository.ChefHierarchiqueRepository;
 import com.pfe.main.service.EmployeService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/main/user")
 public class UserController {
 	
