724c15950c2fcf43b4bf3eade99372128591ba08
tekayakarim <36500892+tekayakarim@users.noreply.github.com>
2020-05-12 03:23:47 +0200

    let's go

---
+++ PlateformeIntranet/src/main/java/com/pfe/main/service/UserService.java
@@ -0,0 +1,10 @@
+package com.pfe.main.service;
+
+import java.util.List;
+
+import com.pfe.main.entity.JwtUser;
+
+public interface UserService {
+List<JwtUser> listerAllUsers();
+String deleteUser(long id);
+}
